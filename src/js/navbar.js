fd0f26e1a4dce39d4842c8dcb90e3335e9ccaee3
Jurgo <jurgo88@gmail.com>
2025-05-07 01:05:54 +0200

    Updated menu and other stuff

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,10 +1,15 @@
-window.addEventListener('scroll', function () {
-  const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 10) {
-    navbar.classList.remove('navbar-top');
-    navbar.classList.add('navbar-scrolled');
-  } else {
-    navbar.classList.remove('navbar-scrolled');
-    navbar.classList.add('navbar-top');
+export function navbar() {
+  console.log("stickyNavbar loaded");
+  document.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".sticky-top");
+  const scrollThreshold = 150;
+  const buffer = 30; 
+  if (window.scrollY > scrollThreshold + buffer) {
+      navbar.classList.add("scrolled");
+  } else if (window.scrollY < scrollThreshold - buffer) {
+      navbar.classList.remove("scrolled");
   }
 });
+}
+navbar();
+
