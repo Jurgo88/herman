8dccaeec268dae206d231a8e7c666f5f04dd356f
Jurgo <jurgo88@gmail.com>
2025-11-04 22:29:17 +0100

    Sticky navbar added

---
+++ src/js/stickyNavbar.js
@@ -0,0 +1,13 @@
+export function stickyNavbar() {
+    console.log("stickyNavbar loaded");
+    document.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".sticky-top");
+    const scrollThreshold = 150;
+    const buffer = 30; 
+    if (window.scrollY > scrollThreshold + buffer) {
+        navbar.classList.add("scrolled");
+    } else if (window.scrollY < scrollThreshold - buffer) {
+        navbar.classList.remove("scrolled");
+    }
+  });
+}
